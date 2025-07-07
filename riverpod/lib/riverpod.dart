f620d6f0c951c026cf0583d90027169b3b94aa92
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2025-07-07 15:33:14 +0700

    feat: add riverpod project

---
+++ riverpod/lib/riverpod.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Riverpod extends StatefulWidget {
+  const Riverpod({super.key});
+
+  @override
+  State<Riverpod> createState() => _RiverpodState();
+}
+
+class _RiverpodState extends State<Riverpod> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
